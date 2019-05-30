8e8febcb41da84f3b344be1169193a4e9711d160
Parth00 <dubalparth@gmail.com>
2019-05-30 01:44:36 -0400

    Update LRUCache.py

--- Q3/LRUCache.py
+++ Q3/LRUCache.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-
+""" Library to implement LRU Cache with Cache expiration"""
 class LRUItem(object):
+    """ Creating a cache item object that contains a key, value and time validity"""
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -9,16 +10,19 @@ class LRUItem(object):
 
 
 class LRUCache(object):
+    """ Creating a cache container that contains its size, list of elements and list of elements in the cache already"""
     def __init__(self, size, time_validity):
         self.time_validity = time_validity
         self.size = size
         self.cache = {}
         self.element_list = []
 
+    #Deleting cache element
     def deleteItem(self, element):
         del self.cache[element.key]
         del self.element_list[self.element_list.index(element)]
 
+    #Pushing cache element to the first if its empty and move to the top if it exists
     def pushElement(self, element):
         if element.key in self.cache:
             index = self.element_list.index(element)
@@ -31,16 +35,12 @@ class LRUCache(object):
             self.cache[element.key] = element
             self.element_list.insert(0, element)
 
+    #Delete cache elements which are expired
     def deleteInvalidElements(self):
         now = datetime.now()
         for element in self.element_list:
             expiration_limit = now - element.time
             if expiration_limit.seconds > self.time_validity:
                 self.deleteItem(element)
-
-    def showCacheElements(self, cache):
-        for key, value in enumerate(cache.element_list):
-            print("Key -> {}, Value -> {}, Time -> {}".format(value.key,value.element,value.time))
-
 
         
